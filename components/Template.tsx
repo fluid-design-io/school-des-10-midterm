0bf297955d2bfd26a7e5982d4d194d93f0dde990
Jiuzhen Pan <2216991777@qq.com>
2023-03-10 23:53:56 -0800

    fix: template file

--- components/Template.tsx
+++ components/Template.tsx
@@ -1,9 +1,35 @@
-import { Template } from "@/components/Template";
+"use client";
 
-export default function Home() {
+import { Button } from "@fluid-design/fluid-ui";
+
+const Heading = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className='min-h-screen bg-zinc-200 dark:bg-zinc-800'>
-      <Template />
-    </main>
+    <h1 className='font-semibold text-zinc-700 dark:text-zinc-300 text-lg uppercase'>
+      {children}
+    </h1>
   );
-}
+};
+
+export const Template = () => {
+  return (
+    <div className='p-4 flex flex-col gap-4'>
+      <Heading>Button</Heading>
+      <div className='flex gap-4 flex-wrap'>
+        <Button color='rose' weight='light' label='Light' />
+        <Button color='rose' weight='normal' label='Normal' />
+        <Button color='rose' weight='bold'>
+          Bold
+        </Button>
+        <Button color='rose' weight='outline'>
+          Outline
+        </Button>
+        <Button color='rose' weight='clear'>
+          Clear
+        </Button>
+        <Button color='rose' weight='link'>
+          Link
+        </Button>
+      </div>
+    </div>
+  );
+};
